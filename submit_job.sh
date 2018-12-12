435b16806b1c6e818584cedc68606fe8bc74ff93
Joao Oliveira <joao.oliveira@semantix.com.br>
2018-12-12 14:15:03 -0200

    fixed submut

--- submit_job.sh
+++ submit_job.sh
@@ -1,40 +1,44 @@
 #!/bin/bash
 
-CONFIG=application.conf
-JARFILE=./test-kafka-consumer-assembly-1.0.0.jar
-APP_NAME="[test-consumer]"
-MAIN_CLASS=br.com.semantix.testkafkaconsumer.ConsumerApp
-KEYTAB=/home/enviotemip/enviotemip.keytab
-JAAS=/home/enviotemip/jaas.conf
+# Configuracoes de ambiente
+APP_NAME="NRTNXTC_AGREGADOR... Joao 10"
 
-spark-submit --class $MAIN_CLASS \
---name $APP_NAME \
---master yarn \
---deploy-mode client \
---driver-memory 1G \
---executor-memory 1G \
---conf spark.executor.cores=1 \
---conf spark.driver.cores=1 \
---conf spark.driver.memoryOverhead=512 \
---conf spark.executor.memoryOverhead=512 \
---conf spark.streaming.backpressure.enabled=true \
---conf spark.streaming.backpressure.pid.minRate=10 \
---conf spark.streaming.backpressure.initialRate=10 \
---conf spark.streaming.kafka.maxRatePerPartition=10 \
---conf spark.streaming.receiver.maxRate=10 \
---conf spark.yarn.maxAppAttemps=4 \
---conf spark.task.maxFailures=4 \
---conf spark.dynamicAllocation.enabled=true \
---conf spark.dynamicAllocation.initialExecutors=3 \
---conf spark.dynamicAllocation.maxExecutors=6 \
---conf spark.dynamicAllocation.minExecutors=3 \
---conf spark.shuffle.service.enabled=true \
---conf spark.shuffle.service.port=7337 \
---files=$CONFIG,$KEYTAB,$JAAS \
---driver-java-options -Dconfig.file=$CONFIG \
---conf "spark.driver.extraJavaOptions=-Dconfig.file=$CONFIG \
--Djava.security.auth.login.config=$JAAS" \
---conf spark.driver.extraClassPath=./ \
---conf spark.executor.extraClassPath=./ \
---conf spark.executor.extraJavaOptions=-Djava.security.auth.login.config=$JAAS \
-$JARFILE
+
+JAR_NAME=test-kafka-consumer-assembly-1.0.11.jar
+
+
+KEYTAB=/data/semantix/enriquecer_categorias/categorizador_dev/dusrbgdt.krb5.keytab
+KEY_CONF=/data/semantix/enriquecer_categorias/categorizador_dev/key.conf
+MASTER=yarn-cluster
+QUEUE=high
+LOG_PROPERTIES=/data/semantix/enriquecer_categorias/categorizador_dev/log4j-spark.properties
+CONFIG=/data/semantix/enriquecer_categorias/categorizador_dev/config_source.properties
+
+# Configuracoes de alocacao do cluster e tolerancia a falhas
+EXECUTOR_CORES=10
+NUM_EXECUTORS=4 #2
+EXECUTOR_MEMORY=8G #4
+DRIVER_MEMORY_OVERHEAD=1024 #512
+EXECUTOR_MEMORY_OVERHEAD=1024 #512
+MAX_APP_ATTEMPTS=4
+MAX_DRIVER_FAILURES=16
+MIN_RATE_PARTITON=10
+MAX_RATE_PARTITON=5000
+
+spark-submit --verbose  \
+            --name "$APP_NAME" \
+            --master $MASTER \
+            --queue $QUEUE \
+            --num-executors $NUM_EXECUTORS \
+            --executor-cores $EXECUTOR_CORES \
+            --executor-memory $EXECUTOR_MEMORY \
+            --conf spark.yarn.driver.memoryOverhead=$DRIVER_MEMORY_OVERHEAD \
+            --conf spark.yarn.executor.memoryOverhead=$EXECUTOR_MEMORY_OVERHEAD \
+            --conf spark.yarn.maxAppAttempts=$MAX_APP_ATTEMPTS \
+            --conf spark.yarn.max.executor.failures=$MAX_DRIVER_FAILURES \
+            --conf spark.streaming.backpressure.enabled=true \
+            --conf spark.streaming.backpressure.pid.minRate=$MIN_RATE_PARTITON \
+            --conf spark.streaming.kafka.maxRatePerPartition=$MAX_RATE_PARTITON \
+            --files $KEY_CONF#key.conf,$KEYTAB,$LOG_PROPERTIES#log4j-spark.properties,$CONFIG#application.properties \
+            --driver-java-options "-Djava.security.auth.login.config=key.conf" \
+            --conf "spark.executor.extraJavaOptions=-Djava.security.auth.login.config=./key.conf -Dsun.security.krb5.debug=true -Dlog4j.configuration=./log4j-spark.properties" $JAR_NAME
